82c0e8bdf70d38b5d3e8538fed9b7ae243c4362c
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-07-03 15:09:21 +0200

    set DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import DeafaultLayout from "./layout/DeafaultLayout"
+
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Routes>
+      <Route Component={DeafaultLayout}>
+
+      </Route>
+    </Routes>
   )
 }
 
